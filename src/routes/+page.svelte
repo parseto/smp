3ac7d90dc279b12175c13e46b653859bbcf9a990
G-yhlee <yhleesoft@gmail.com>
2024-08-26 09:05:40 +0900

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    goto("/dashboard");
+  });
 </script>
 
 <div>INDEX</div>
